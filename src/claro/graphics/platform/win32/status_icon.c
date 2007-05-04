a214d5984be528bd307ed7451c6ae065202e90e8
Theo Julienne <theo.julienne@gmail.com>
2007-05-04 07:55:08 +0000

    * added stubs for status_icon on win32/cocoa  * changed spaces to tabs so cbuild doesn't get confused

---
+++ src/claro/graphics/platform/win32/status_icon.c
@@ -0,0 +1,44 @@
+/* Claro Graphics - an abstraction layer for native UI libraries
+ * 
+ * $Id$
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * See the LICENSE file for more details.
+ */
+
+
+#include "../win32.h"
+
+void cgraphics_status_icon_create(status_icon_t * status, int flags)
+{
+	
+}
+
+void cgraphics_status_icon_set_icon(status_icon_t * status, image_t * icon)
+{
+	
+}
+
+void cgraphics_status_icon_set_menu(status_icon_t * status, object_t * menu)
+{
+	
+}
+
+void cgraphics_status_icon_set_visible(status_icon_t * status, int visible)
+{
+	
+}
+
+void cgraphics_status_icon_set_tooltip(status_icon_t * status, const char * tooltip)
+{
+	
+}
