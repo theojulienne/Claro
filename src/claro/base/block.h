1fc921288073f22bf3494206c25167585738102e
Theo Julienne <theo.julienne@gmail.com>
2007-08-01 23:44:27 +0000

    Removing more code as per r470

--- src/claro/base/block.h
+++ src/claro/base/block.h
@@ -19,64 +19,6 @@
 #ifndef _CLARO_BASE_BLOCK_H
 #define _CLARO_BASE_BLOCK_H
 
-/* 
-
-typedef struct block_ block_t;
-typedef struct block_heap_ block_heap_t;
-
-// A block of memory allocated to the allocator 
-struct block_
-{
-	node_t node;
-	
-	// link back to our heap 
-	block_heap_t *heap;
-	
-	// pointer to the first item 
-	void *data;
-	
-	// lists of items 
-	list_t free_list;
-	list_t used_list;
-};
-
-// A pile of blocks 
-struct block_heap_
-{
-	node_t node;
-	
-	uint32 elem_size;
-	uint32 block_elems;
-	uint32 free_elems;
-	
-	uint32 alloc_size;
-	
-	uint32 flags;
-	
-	list_t blocks;
-};
-
-// List of all block heaps 
-extern list_t block_heaps;
-
-// Flag for block_heap_create 
-#define BH_DONTCARE 0
-
-#define BH_NOW 1
-#define BH_LAZY 0
-
-void block_init( );
-
-// Functions for heaps 
-CLFEXP block_heap_t *block_heap_create( uint32 elem_size, uint32 block_elems, uint32 flags );
-CLFEXP void block_heap_destroy( block_heap_t *heap );
-
-// Functions for blocks 
-CLFEXP void *block_alloc( block_heap_t *heap );
-CLFEXP void block_free( block_heap_t *heap, void *data );
-
-*/
-
 CLFEXP void block_heap_loginfo( );
 
 
